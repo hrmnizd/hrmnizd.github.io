ba1411a39caf2ed32f1dccb476d1f4c1355b040b
hrmnizd <putsch.frog-0o@icloud.com>
2024-07-13 20:42:30 -0600

    hi

---
+++ script.js
@@ -0,0 +1,11 @@
+window.addEventListener("scroll", function() {
+    const header = document.querySelector(".global-header");
+    const headerHeight = header.offsetHeight; // Get header height
+    const scrollY = window.scrollY; // Get scroll position
+  
+    if (scrollY > headerHeight) {
+      header.classList.add("hidden"); // Add a hidden class to the header
+    } else {
+      header.classList.remove("hidden");
+    }
+  });
