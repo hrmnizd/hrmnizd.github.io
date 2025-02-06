436c96d0b6e3a3cfa49af829ccb5ebd03ac8a1b1
hrmnizd <putsch.frog-0o@icloud.com>
2025-02-05 17:59:18 -0800

    🫧

--- js/header.js
+++
@@ -1,9 +0,0 @@
-const header = document.getElementById("idh");
-
-function openNav() {
-    header.classList.add("active-nav");
-}
-
-function closeNav() {
-    header.classList.remove("active-nav");
-}
